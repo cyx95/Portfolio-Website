bcc5acda4ea76771f5dff8368307cd0ef4b13125
Chen Ying Xin <chenyingxin92@gmail.com>
2022-06-09 16:30:34 -0400

    fixed padding in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,13 @@ import "../App.css";
 function Home() {
   return (
     <div className="home-page">
-        <p className="home-info">
+      <div className="home-info">
+        <p>
           Hi! I'm a Software Engineer
           <br></br> based in New York City specializing
           <br></br>in Fullstack Development.
         </p>
+      </div>
     </div>
   );
 }
