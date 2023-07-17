d3db929582b8e7a122480c3d12affab620855a62
Chen Ying Xin <chenyingxin92@gmail.com>
2023-07-17 17:06:55 -0400

    updated projects

--- src/projects.js
+++ src/projects.js
@@ -1,18 +1,18 @@
 
 const projectData = [
     {
+        title: "Pokemon API",
+        picture: "https://twinfinite.net/wp-content/uploads/2016/02/pokemon_starters.png?resize=600%2C300",
+        description: "Developed an API containing a comprehensive collection of the initial 151 Pokemon. Developed using a tech stack encompassing JavaScript, HTML, CSS, React, Node, Express and CI/CD tools.",
+        github: "https://github.com/Original-Pokedex/Original-Pokedex",
+        deployedSite: "https://original-pokedex-production-e65d.up.railway.app/"
+    },
+    {
         title: "Haunted Mansion",
         picture: "https://user-images.githubusercontent.com/96203831/161592220-83f84c1c-9618-402b-8c6f-94c4e2d01088.jpg",
         description: "A 2D point and click adventure using Kaboom.js. The game starts with the player waking up and finding themselves in the basement of an abandoned mansion. The player would need to navigate the whole mansion looking for clues to escape, while avoiding an evil ghost. Friendly ghosts are placed throughout the mansion to help you escape.",
         github: "https://github.com/Team-Grape/CapstoneProject",
         deployedSite: "https://team-grape.github.io/CapstoneProject/"
-    },
-    {
-        title: "Planet Wine",
-        picture: "https://altobone-electrikproject.s3.amazonaws.com/wp-content/uploads/2018/12/22133151/Planet-background-for-sleep-music-1.jpg",
-        description: "An e-commerce website that sells wine specific to the Napa Valley Region.",
-        github: "https://github.com/Uranus-Grace-Shopper/GraceShopper",
-        deployedSite: "https://uranus-wine.herokuapp.com/"
     },
 ]
 
